11f7fb2d1a767acb728bbe948d16f15a8a626f64
DayakarReddy07 <reddygarudayakar@gmail.com>
2023-08-30 22:31:46 +0530

    optimized problems

---
+++ DSA practice/Divide and conquer/majorityElements.java
@@ -0,0 +1,29 @@
+public class majorityElements {
+    private static int countInRange(int[] nums,int num,int lo,int hi){
+        int count = 0;
+        for(int i=0;i<=hi;i++){
+            if(nums[i] == num){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static int majorityElementRec(int nums[],int lo,int hi){
+        if(lo==hi){
+            return nums[lo];
+        }
+        int mid = (hi-lo)/2 + lo;
+        int left = majorityElementRec(nums, lo, mid);
+        int right = majorityElementRec(nums, mid+1, hi);
+        if(left == right){
+            return left;
+                }
+        int leftCount = countInRange(nums, left, lo, hi);
+        int rightCount = countInRange(nums, right, lo, hi);
+        return leftCount>rightCount?left:right;
+    }
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(majorityElementRec(nums, 0, nums.length-1));
+    }
+}
