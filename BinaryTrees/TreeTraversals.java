030c57e5e76f35c1ed3a255898f4611d03d3fa53
DayakarReddy07 <111077329+DayakarReddy07@users.noreply.github.com>
2023-09-29 13:15:29 +0530

    Binary Trees basic problems solved

---
+++ BinaryTrees/TreeTraversals.java
@@ -0,0 +1,98 @@
+package DSA_90Days_Practice.BinaryTrees;
+import java.util.*;
+public class TreeTraversals {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+        static class BinaryTree{
+            static int idx = -1;
+            public Node buildTree(int nodes[]){
+                idx++;
+                if(nodes[idx] == -1){
+                    return null;
+                }
+                Node newNode = new Node(nodes[idx]);
+                newNode.left = buildTree(nodes);
+                newNode.right = buildTree(nodes);
+                return newNode;
+            }
+
+            public void preOrder(Node root){
+                if(root == null){
+                    return ;
+                }
+                System.out.print(root.data + " ");
+                preOrder(root.left);
+                preOrder(root.right);
+            }
+
+            public void inOrder(Node root){
+                if(root == null){
+                    return;
+                }
+                inOrder(root.left);
+                System.out.print(root.data + " ");
+                inOrder(root.right);
+            }
+
+            public void postOrder(Node root){
+                if(root == null){
+                    return;
+                }
+                postOrder(root.left);
+                postOrder(root.right);
+                System.out.print(root.data + " ");
+            }
+
+            public void levelOrder(Node root){
+                if(root == null){
+                    return;
+                }
+                Queue<Node> q = new LinkedList<>();
+                q.add(root);
+                q.add(null);
+                while(!q.isEmpty()){
+                    Node currNode = q.remove();
+                    if(currNode == null){
+                        System.out.println();
+                        if(q.isEmpty()){
+                            break;
+                        }else{
+                            q.add(null);
+                        }
+                    } else{
+                        System.out.print(currNode.data + " ");
+                        if(currNode.left != null){
+                            q.add(currNode.left);
+                        }
+                        if(currNode.right != null){
+                            q.add(currNode.right);
+                        }
+                    }
+                }
+            }
+
+        }
+    public static void main(String[] args) {
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.print("The Pre order travesal of a tree is : ");
+        tree.preOrder(root);
+        System.out.println();
+        System.out.print("The In Order traversal of a tree is : ");
+        tree.inOrder(root);
+        System.out.println();
+        System.out.print("The Post order traversal of a tree is : ");
+        tree.postOrder(root);
+        System.out.println("The level order traversal of a tree is : ");
+        tree.levelOrder(root);
+     }
+}
